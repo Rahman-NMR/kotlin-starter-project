93689dea0b53a652483f57b1fab38f34d7e77623
Rahman <49480009+Rahman-NMR@users.noreply.github.com>
2026-05-13 22:42:04 +0700

    update logical fizzbuzz
    exclude prime number from output

--- src/main/kotlin/fizzbuzz/main.kt
+++ src/main/kotlin/fizzbuzz/main.kt
@@ -12,10 +12,26 @@ fun main() {
 }
 
 fun generateFizzBuzzValue(number: Int): String? {
+    if (isPrime(number)) return null
+
     return when {
         number % 15 == 0 -> "FooBar"
         number % 3 == 0 -> "Foo"
         number % 5 == 0 -> "Bar"
         else -> number.toString()
     }
+}
+
+fun isPrime(number: Int): Boolean {
+    if (number <= 1) return false
+    if (number <= 3) return true
+    if (number % 2 == 0 || number % 3 == 0) return false
+
+    var divisor = 5
+    while (divisor <= number / divisor) {
+        if (number % divisor == 0 || number % (divisor + 2) == 0) return false
+        divisor += 6
+    }
+
+    return true
 }
